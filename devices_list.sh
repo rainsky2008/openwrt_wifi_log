7929f8d1d2bc8ed24a069847bb8230671b40bb48
rainsky2008 <rainsky2008@users.noreply.github.com>
2016-04-07 20:25:07 +0800

    Create devices_list.sh

---
+++ devices_list.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+data=''
+current=`date "+%Y-%m-%d %H:%M:%S"`
+timeStamp=`date -d "$current" +%s`
+for ip in $(cat /proc/net/arp | grep -v IP|grep -v 0x0| awk '{print $1}'); do
+    client=$(grep $ip /tmp/dhcp.leases |awk '{print $1","$2","$3","$4}')
+    if [ -z $client ]; then
+        data=$data"_ZV_"$timeStamp","$(cat /proc/net/arp|grep $ip|awk '{print $4}')","$ip",unknow"
+    else
+        data=$data"_ZV_"$client
+    fi
+done
+echo $data
+curl 'http://123.56.154.0:8080/zrdm2x/router/data?key=hifiwifi_001&data='$data
